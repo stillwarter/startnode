1c74cc2c9ab96cec8d966b7f605bfd2d0f08c81f
stillwarter <2406017193@qq.com>
2024-01-13 10:18:16 +0800

    up

--- vue-workplate/src/view/lib/base.vue
+++ vue-workplate/src/view/lib/base.vue
@@ -4,7 +4,7 @@ import { ref } from "vue";
 import { Md5 } from "ts-md5";
 import axios from "axios";
 import { showMessage } from "@/tool/message";
-import { fishwords } from "./data/words.js";
+import { fishwords, fishwordszhiyi } from "./data/words.js";
 const md5 = new Md5();
 const password = ref(".....");
 const getKey = () => {
@@ -22,23 +22,55 @@ const getKey = () => {
   });
 };
 
+// const a = localStorage.getItem("fishkey");
+// console.log(a);
+
 const sdata = {
   apiKey: localStorage.getItem("fishkey"),
   client: "Windows/V1.0.0",
   content: "",
 };
 const sendMessage = (word) => {
+  // sdata.content =
+  //   word +
+  //   "\n" +
+  //   "---" +
+  //   "\n" +
+  //   "> <sub>❤️现在是代理喵520号机器人❤️~ 请勿打扰</sub>" +
+  //   "\n" +
+  //   "<sub>send by StartNode  Cover:cat520</sub>";
   sdata.content =
     "\n" +
-    "###" +
-    " " +
     word +
-    "\n" +
-    "---" +
     "\n" +
     "> <sub>❤️现在是代理喵520号机器人❤️~ 请勿打扰</sub>" +
     "\n" +
-    "<sub>send by StartNode  Cover:cat520</sub>";
+    "<sub>~~~send by StartNode  Cover:cat520</sub>";
+  axios.post("/chat-room/send", sdata).then((res) => {
+    if (res.status === 200) {
+      showMessage("发送成功");
+    }
+  });
+};
+
+const sendMessagewithzhiyi = (word) => {
+  // sdata.content =
+  //   word +
+  //   "\n" +
+  //   "---" +
+  //   "\n" +
+  //   "> <sub>❤️现在是代理喵520号机器人❤️~ 请勿打扰</sub>" +
+  //   "\n" +
+  //   "<sub>send by StartNode  Cover:cat520</sub>";
+  sdata.content =
+    "\n" +
+    word +
+    "\n" +
+    "--- 《摸鱼志异》" +
+    "\n" +
+    "> <sub>❤️现在是代理喵520号机器人❤️~ 请勿打扰</sub>" +
+    "\n" +
+    "<sub>~~~send by StartNode  Cover:cat520</sub>";
   axios.post("/chat-room/send", sdata).then((res) => {
     if (res.status === 200) {
       showMessage("发送成功");
@@ -51,21 +83,31 @@ const sendMessage = (word) => {
   <div class="libbox">
     <div class="leftmeun">
       this is lefmeun:
-      <div style="margin-left: 40px;">fish</div>
+      <div style="margin-left: 40px">fish</div>
     </div>
     <div class="content">
       <stinput label="password" v-model:contentvalue="password" />
       <div class="btn" @click="getKey">sure</div>
       <div class="btn" @click="sendMessage">sendMessage</div>
-      <p>
-        fish的闲言碎语
-      </p>
+      <p>fish的闲言碎语</p>
       <div class="word">
         <div
           class="card"
           v-for="(item, index) in fishwords"
           :key="index"
           @click="sendMessage(item)"
+        >
+          {{ item }}
+        </div>
+      </div>
+
+      <p>摸鱼志异</p>
+      <div class="word">
+        <div
+          class="card"
+          v-for="(item, index) in fishwordszhiyi"
+          :key="index"
+          @click="sendMessagewithzhiyi(item)"
         >
           {{ item }}
         </div>
