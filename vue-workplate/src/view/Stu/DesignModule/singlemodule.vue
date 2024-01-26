d03737ede5810a45d737846d4f36f855fc936ada
stillwarter <2406017193@qq.com>
2024-01-26 17:22:48 +0800

    Stu updata

--- vue-workplate/src/view/Stu/DesignModule/singlemodule.vue
+++ vue-workplate/src/view/Stu/DesignModule/singlemodule.vue
@@ -3,7 +3,7 @@ import stQuote from "@/components/Card/st-quote.vue";
 import {
   singlemodulehead,
   singlemodulecode,
-  singlemoduletrancode,
+  singlemoduleagentcode,
 } from "../data/singlemodule";
 
 var CreateDiv = (function () {
@@ -33,14 +33,14 @@ console.log(instance1, instance2, instance1 === instance2);
 <template>
   <div class="commbox">
     <div>
-      <h2>2.单例模式</h2>
+      <h2>1.单例模式</h2>
       <div>
         <p>前言：</p>
         <stQuote class="mg-b10" :ctx="singlemodulehead" />
       </div>
 
       <div>
-        <h3>2.1 单例设计模式的实现</h3>
+        <h3>1.1 单例设计模式的实现</h3>
         <pre>
           {{ singlemodulecode }}
         </pre>
@@ -53,7 +53,7 @@ console.log(instance1, instance2, instance1 === instance2);
       </div>
 
       <div>
-        <h3>2.2 透明的单例设计模式</h3>
+        <h3>1.2 透明的单例设计模式</h3>
         <div>
           为了解决new构造的方式无法实现单例问题，所以有透明单例模式的说法：
           <pre>
@@ -61,6 +61,21 @@ console.log(instance1, instance2, instance1 === instance2);
           </pre>
           这个例子返回一个creatediv的构造函数，当我们使用这个creatediv时候，
           由于闭包的缘故，所以在第一次初始化的时候，instance的值就被固定了。这时候，我们new生成的实例就是唯一的。
+        </div>
+      </div>
+
+      <div>
+        <h3>1.3 代理的单例设计模式</h3>
+        <div>
+          虽然上述透明的单例设计模式解决了不用通过Singleton.getInstance来获取单例类的唯一对象，
+          但是在透明的单例设计模式中，构造函数CreateDiv违反了单一职责，它不仅要负责创建对象，
+          而且还要负责保证单例，假如某一天需求变化了，不再需要创建单例的div，
+          则需要改写CreateDiv函数，解决这种问题，可以使用代理来实现单例模式
+          <pre>
+            {{ singlemoduleagentcode }}
+          </pre>
+          这个例子拆分了透明单例中的部分，将creatediv单独出来，用proxycreatediv进行单例的设置（instance）
+          这样就不需要再改写所有代码，只需呀处理create构造函数
         </div>
       </div>
     </div>
--- vue-workplate/src/view/Stu/data/singlemodule.js
+++ vue-workplate/src/view/Stu/data/singlemodule.js
@@ -1,5 +1,6 @@
 export const singlemodulehead = `单例模式的定义是： 保证一个类仅有一个实例，并提供一个访问它的全局访问点。
 单例模式是一种常用的模式，有一些对象我们往往只需要一个，比如线程池、全局缓存、浏览器中 的 window 对象等。在 JavaScript 开发中，单例模式的用途同样非常广泛。试想一下， 当我们单击登录按钮的时候，页面中会出现一个登录浮窗，而这个登录浮窗是唯一的，无论单击 多少次登录按钮，这个浮窗都只会被创建一次，那么这个登录浮窗就适合用单例模式来创建。
+其他设计案例：线程池，全局缓存，window对象...
 `;
 
 export const singlemodulecode = `
@@ -51,3 +52,28 @@ var CreateDiv = (function(){
 var instance1 = new CreateDiv('why');
 var instance2 = new CreateDiv('www');
 console.log(instance1===instance2); // 输出true`;
+
+export const singlemoduleagentcode = `
+// 用代理实现单例模式
+var CreateDiv = function(html) {
+  this.html = html;
+  this.init();
+}
+CreateDiv.prototype.init = function() {
+  var div = document.createElement('div');
+  div.innerHTML = this.html;
+  document.body.appendChild(div);
+}
+var ProxyCreateDiv = (function(){
+  var instance = null;
+  return function(html) {
+    // 惰性单例
+    if(!instance) {
+      instance = new CreateDiv(html);
+    }
+    return instance;
+  }
+})()
+var divInstance1 = new ProxyCreateDiv('why');
+var divInstance2 = new ProxyCreateDiv('www');
+console.log(divInstance1===divInstance2); // 输出true`;
